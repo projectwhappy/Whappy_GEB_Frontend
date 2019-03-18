dda9bf3925143decad622b098e23db8ebd7766cb
Luca Bianco <luca.bianco@raintonic.com>
2019-03-18 10:49:17 +0100

    Added module for tablet and basic config for repository

---
+++ src/app/modules/tablet/tablet.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TabletRoutingModule } from './tablet-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TabletRoutingModule
+  ]
+})
+export class TabletModule { }
