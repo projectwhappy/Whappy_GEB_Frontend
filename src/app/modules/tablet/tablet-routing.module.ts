4ea77051d0febfd4acaa86225e0e67a5253b690c
DanieleArtusi <artusi.daniele@gmail.com>
2019-03-19 15:41:27 +0100

    Add Nav html Template
    Added a mock-up of the navigation menu in choose-store page

--- src/app/modules/tablet/tablet-routing.module.ts
+++ src/app/modules/tablet/tablet-routing.module.ts
@@ -10,11 +10,11 @@ const routes: Routes = [
     component: TabletComponent,
     children:[
       {
-        path: '',
+        path: '/event',
         component: EventManagerComponent,
       },
       {
-        path: '/chooseStore',
+        path: '',
         component: ChooseStoreComponent,
 
       }
