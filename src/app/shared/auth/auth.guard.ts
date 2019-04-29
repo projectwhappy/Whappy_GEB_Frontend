cd25e6f1f2bb15ea0cca7e20f3a009f2eb677495
VladWhappy <vlad-whappy@outlook.it>
2019-04-29 16:33:36 +0200

    Fixed authentication roles bug

--- src/app/shared/auth/auth.guard.ts
+++ src/app/shared/auth/auth.guard.ts
@@ -24,7 +24,7 @@ export class AuthGuard implements CanActivate {
             }
 
             // check if route is restricted by role
-            if (route.data.roles && route.data.roles.indexOf(currentUser.role)) {
+            if (route.data.roles.indexOf(currentUser.role) === -1) {
                 // role not authorised so redirect to home page
                 this.router.navigate(['/' + currentUrlSegment + '/'], { queryParams: { returnUrl: state.url }});
                 return false;
