64bf756b7e80e9ccdea745e930db568386f28800
VladWhappy <vlad-whappy@outlook.it>
2019-05-09 09:49:13 +0200

    - Added role to navbar

--- src/app/modules/admin/components/nav/nav.component.ts
+++ src/app/modules/admin/components/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {AuthenticationService} from '../../../../core/http/authentication.service';
 
+type CurrentUser = { token: string, mail: string, role: string};
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -10,15 +12,15 @@ export class NavComponent implements OnInit {
 
   @Input() public isNavOpen: boolean;
   @Output() public closeNav = new EventEmitter();
-  public currentUserMail:string;
+  public currentUser:CurrentUser;
 
   constructor(private authenticationService: AuthenticationService) {
   }
 
   ngOnInit() {
     let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    this.currentUserMail = ((currentUser.mail) ? currentUser.mail : 'Guest');
-    this.currentUserMail = currentUser.mail;
+    this.currentUser = currentUser;
+    this.currentUser.mail = ((currentUser.mail) ? currentUser.mail : 'Guest');
   }
 
   public sendCloseNav() {
